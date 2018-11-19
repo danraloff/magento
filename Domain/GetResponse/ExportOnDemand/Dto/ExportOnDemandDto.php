cc979e6e1ff7a10374e94ba0e51b9c9a1a563aed
bstepien <bstepien@getresponse.com>
2018-11-19 08:42:00 +0100

    PostCrFixes

--- Domain/GetResponse/ExportOnDemand/Dto/ExportOnDemandDto.php
+++ Domain/GetResponse/ExportOnDemand/Dto/ExportOnDemandDto.php
@@ -67,7 +67,7 @@ class ExportOnDemandDto
             isset($requestData['gr_autoresponder']),
             (isset($requestData['gr_autoresponder']) && $requestData['cycle_day'] !== '') ? (int)$requestData['cycle_day'] : null,
             isset($requestData['ecommerce']) && !empty($requestData['ecommerce']),
-            (isset($requestData['ecommerce']) && !empty($requestData['ecommerce'])) ? $requestData['store_id'] : null,
+            (isset($requestData['ecommerce']) && !empty($requestData['ecommerce']) && !empty($requestData['store_id'])) ? $requestData['store_id'] : null,
             isset($requestData['gr_sync_order_data']),
             CustomFieldMappingDtoCollection::createFromRequestData($requestData)
         );
